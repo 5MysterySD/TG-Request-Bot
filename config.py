1eb620959554900145c4f8076cd9370bcec81e38
MysterySD <91897989+MysteryCodeSD@users.noreply.github.com>
2022-05-06 14:44:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5347120398:AAFKgxPGUF6jjTqSf4h5RPlp_DYEG_baA2Y")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("APP_ID", "6878048"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "3833ae3a7415af46df46a83a3ba2c432")
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1789263079"))
 
-LOG_FILE_NAME = "filesharingbot.txt"
+LOG_FILE_NAME = "Requestbot.txt"
 
 logging.basicConfig(
     level=logging.INFO,
