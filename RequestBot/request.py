d6984b6f3280b3e3544fa690f1f9d58679d1934c
MysterySD <91897989+MysteryCodeSD@users.noreply.github.com>
2022-05-07 18:28:10 +0530

    Update request.py

--- RequestBot/request.py
+++ RequestBot/request.py
@@ -2,10 +2,10 @@
 
 from pyrogram import Client, filters, __version__
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from RequestBot.bot import Client
+from RequestBot.bot import RequestBot
 from config import OWNER_ID
 
-@Client.on_message(filters.command('start'))
+@RequestBot.on_message(filters.command('start'))
 async def start_command(client: Client, message: Message):
     id = message.from_user.id
     chatid = message.chat.id 
