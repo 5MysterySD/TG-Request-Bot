c0e6b8b3ad8365141a35fd2a24a5406a5651cf41
MysterySD <91897989+MysteryCodeSD@users.noreply.github.com>
2022-05-07 18:54:31 +0530

    Update request.py

--- RequestBot/request.py
+++ RequestBot/request.py
@@ -3,10 +3,13 @@
 from pyrogram import Client, filters, __version__
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from RequestBot.bot import RequestBot
-from config import OWNER_ID
+from config import OWNER_ID, LOGGER 
 
 @RequestBot.on_message(filters.command('start'))
 async def start_command(client: Client, message: Message):
-    id = message.from_user.id
-    chatid = message.chat.id 
-    start = message.reply_text("Request Assigned")
+    try:
+        id = message.from_user.id
+        chatid = message.chat.id 
+        start = message.reply_text("Request Assigned")
+    except Exception as e:
+        LOGGER.getLogger(__name__).error(err)
